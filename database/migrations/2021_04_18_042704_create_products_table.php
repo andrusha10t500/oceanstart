053e2c1a36834a97a28315d37e2e4a00b3807d38
andrusha10t500 <andrusha10t500@gmail.com>
2021-04-18 14:38:08 +1000

    create migrations, models, controllers

---
+++ database/migrations/2021_04_18_042704_create_products_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->timestamps();
+            $table->string('name');
+            $table->integer('category')->nullable();
+            $table->string('category_name')->nullable();
+            $table->string('price');
+            $table->boolean('public');
+            $table->boolean('deleted');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
